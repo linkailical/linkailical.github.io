9b72f3b0857782be8258495498d571dbe70d1452
Linkaili <linkaili.2008@gmail.com>
2026-01-02 10:57:44 +0800

    Refactor website to Academic Portfolio structure

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Linkaili | Personal Portfolio';
-export const SITE_DESCRIPTION = 'Personal portfolio website of Linkaili, a high school student athlete in the US.';
+export const SITE_TITLE = 'Linkaili | Academic Portfolio & Profile';
+export const SITE_DESCRIPTION = 'Academic portfolio of Linkaili, a high school student-athlete (Class of 2027) focused on Science, Research, and Competitive Golf.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
